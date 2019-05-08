3cca8267933b960af2bd29fa383c1b2634efad63
YaliW <yw1976@nyu.edu>
2019-05-08 20:52:02 +0800

    feat: fix pages issue

--- src/views/Home/Paging.vue
+++ src/views/Home/Paging.vue
@@ -56,11 +56,11 @@
 <template>
     <div class="wrapper">
         <div class="paging-container">
-            <a class="page prev-page">上一页</a>
-            <a class="num-page" v-for="(index, page) in pagingArr" v-bind:key="index">{{page+1}}</a>
+            <a class="page prev-page">上一页</a >
+            <a class="num-page" v-for="(index, page) in pagingArr" v-bind:key="index">{{page+1}}</a >
             <span>...</span>
-            <a class="num-page">{{Math.ceil(tempData / limit)}}</a>
-            <a class="page next-page">下一页</a>
+            <a class="num-page">{{totalPages}}</a >
+            <a class="page next-page">下一页</a >
         </div>
     </div>
 </template>
@@ -69,28 +69,30 @@
 export default {
     name: 'Paging',
     data() {
-        console.log(this.data, 'totalPlay from Paging');
+        // 这里是类似 constructor 的初始化过程，因此只能写初始值
         return {
-            pagingArr: [1, 2, 3, 4, 5],
-            tempData: 1288,
             limit: 35,
-
+            offset: 0
         }
-        
     },
     computed: {
         totalPlay: function() {
-            console.log(this.data);
-            return this.tempData;
+            return this.data;
         },
         totalPages: function() {
-            console.log(this.tempData / this.limit);
-
-            return this.tempData / this.limit;
+            console.log(this.data);
+            // 这里可以对 props 的值进行处理，也可以直接使用props中的值在template中绑定
+            // 数据处理逻辑尽量放到这里，不要写到模板里
+            return Math.ceil(this.data / this.limit);
+        },
+        pagingArr: function() {
+            let arr = [];
+            for (let i = 0; i < Math.ceil(this.data / this.limit); i++) {
+                arr.push(i+1);
+            }
+            return arr;
         }
     },
-    props: ['data'],
-
-    
+    props: ['data']
 }
 </script>
