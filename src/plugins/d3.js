d08e36b7378d2da227f565b29d7b56b1808b28d2
YaliW <yw1976@nyu.edu>
2019-09-02 17:29:50 +0800

    add d3 example

--- src/plugins/d3.js
+++ src/plugins/d3.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 
 // 可以选择使用npm来引入D3而不是在html中引入D3的源文件
-// const D3 = typeof require === 'function' ? require('d3') : window.d3;
-const D3 = window.d3;
+const D3 = typeof require === 'function' ? require('d3') : window.d3;
+// const D3 = window.d3;
 
 if (!D3) {
   throw new Error('[vue-d3] cannot locate d3');
--- src/views/D3/index.vue
+++ src/views/D3/index.vue
@@ -1,17 +1,98 @@
 <style lang="less">
-  
+  .line-chart {
+    font: 10px sans-serif;
+  }
+
+  .axis path,
+  .axis line {
+    fill: none;
+    stroke: #000;
+    shape-rendering: crispEdges;
+  }
+
+  .line {
+    fill: none;
+    // stroke: steelblue;
+    stroke: red;
+
+    stroke-width: 1.5px;
+  }
+
+  h1 {
+    color: red;
+  }
 </style>
+
 <template lang="html">
   <div class='d3-container'>
     专业D3工程师
   </div>
 </template>
+
 <script>
+import LineChart from './LineChart';
+
 export default {
   data() {
     return {
       data: []
     };
+  },
+  mounted() {
+    this.run();
+  },
+  methods: {
+    run() {
+      console.log("The application is running now ...");
+
+        const initData = [
+            { month: 0, value: 5},
+            { month: 1, value: 5},
+            { month: 2, value: 5},
+            { month: 3, value: 5},
+            { month: 4, value: 5},
+            { month: 5, value: 5},
+            { month: 6, value: 5},
+            { month: 7, value: 5},
+            { month: 8, value: 5},
+            { month: 9, value: 5},
+            { month: 10, value: 5},
+            { month: 11, value: 5},
+            { month: 12, value: 5}
+        ];
+        const data = [
+            { month: 0, value: 2},
+            { month: 1, value: 4},
+            { month: 2, value: 5},
+            { month: 3, value: 6},
+            { month: 4, value: 6},
+            { month: 5, value: 9},
+            { month: 6, value: 6},
+            { month: 7, value: 4},
+            { month: 8, value: 3},
+            { month: 9, value: 0},
+            { month: 10, value: 4},
+            { month: 11, value: 6},
+            { month: 12, value: 3}
+        ];
+
+        const lineChart = new LineChart({
+            width: 700,
+            height: 400,
+            margin: { top: 20, right: 30, bottom: 20, left: 30 },
+            initData,
+            data,
+            xPath: "month",
+            yPath: "value"
+        });
+
+        setTimeout(() => {
+            data[5]["value"] = 6;
+            data[9]["value"] = 2;
+
+            lineChart.render();
+        }, 1000);
+    }
   }
 }
 </script>
